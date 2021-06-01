c33d4eb48eed92b83733edadd929b57baa9a3850
ILLIA Kahanov <kahanov.illia@gmail.com>
2021-06-01 23:30:07 +0300

    module_2 done!

--- module_2.js
+++ module_2.js
@@ -349,3 +349,253 @@ for (i = 0; i < order.length; i += 1) {
 }
 calculateTotalPrice([412, 371, 94, 63, 176]);
 calculateTotalPrice([12, 85, 37, 4]);
+
+// 2-21
+// Напиши функцию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом(параметр string)
+// и возвращает самое длинное слово в этой строке
+
+function findLongestWord(string) {
+  // Change code below this line
+
+const array = string.split(" ");
+
+let longestWord = array[0];
+
+
+for (i = 0; i < array.length; i +=1) {
+    if (longestWord.length < array[i].length) {
+      longestWord = array[i];
+    }
+}
+  return longestWord;
+
+  // Change code above this line
+}
+
+// 2-22
+// Дополни код функции createArrayOfNumbers(min, max) так, чтобы она возвращала массив всех целых чисел от значения min до max.
+
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+	for (i = min; i <= max; i +=1) {
+     numbers.push(i); 
+    }
+  // Change code above this line
+  return numbers;
+}
+createArrayOfNumbers(29, 34);
+createArrayOfNumbers(1, 3);
+
+// 2-23
+// Напиши функцию filterArray(numbers, value), которая принимает массив чисел(параметр numbers) и возвращает новый массив,
+// в котором будут только те элементы массива numbers, которые больше чем значение параметра value(число).
+
+function filterArray(numbers, value) {
+   // Change code below this line
+const array = [];
+  for (i = 0; i < numbers.length; i +=1) {
+  if (numbers[i] > value) {
+    array.push(numbers[i]);
+  }
+}
+return array;
+
+  // Change code above this line
+}
+
+filterArray([1, 2, 3, 4, 5], 5);
+filterArray([12, 24, 8, 41, 76], 20);
+
+// 2-24
+// Функция checkFruit(fruit) принимает строку с названием фрукта (параметр fruit), и проверяет есть ли такой фрукт в массиве fruits.
+
+// Дополни код функции так, что если:
+
+// фрукт есть в массиве, то функция возвращает true;
+// фрукта нет в массиве, то функция возвращает false.
+
+function checkFruit(fruit) {
+  const fruits = ["apple", "plum", "pear", "orange"];
+
+  return fruits.includes(fruit); // Change this line
+}
+
+checkFruit("mandarin");
+checkFruit("Pear");
+checkFruit("apple");
+
+// 2-25
+// Общими элементами массивов называют те элементы, которые присутствуют во всех массивах.
+
+// Например, в двух массивах[1, 3, 5] и[0, 8, 5, 3] общими будут числа 3 и 5, т.к.они присутствуют в обоих исходных массивах.
+// А числа 0, 1 и 8 присутствуют только в одном из массивов.
+
+// Напиши функцию getCommonElements(array1, array2) которая получает два массива произвольной длины в параметры array1 и array2,
+// и возвращает новый массив, состоящий из тех элементов, которые присутствуют в обоих исходных массивах.
+
+function getCommonElements(array1, array2) {
+  // Change code below this line
+const array = [];
+  
+  for (i = 0; i < array1.length; i +=1) {
+    if (array2.includes(array1[i])) {
+        array.push(array1[i])
+    }
+  }
+  
+  return array;
+
+ // Change code above this line
+}
+
+getCommonElements([1, 2, 3], [10, 20, 30]);
+getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]);
+
+
+// 2-26
+// Выполни рефакторинг кода функции calculateTotalPrice(order) заменив цикл for на for...of.
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+
+  for (const number of order) {
+    total += number;
+  }
+
+  // Change code above this line
+  return total;
+}
+
+calculateTotalPrice([412, 371, 94, 63, 176]);
+calculateTotalPrice([12, 85, 37, 4]);
+
+// 2-27
+// Выполни рефакторинг функции filterArray(numbers, value) заменив цикл for на for...of.
+
+function filterArray(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
+
+  for (const item of numbers) {
+    const number = item;
+
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
+  // Change code above this line
+}
+
+// 2-28
+// Дополни выражения остатка от деления так, чтобы код проходил тесты.
+// Значение переменной a это число 0
+// Значение переменной b это число 1
+// Значение переменной c это число 3
+// Значение переменной d это число 5
+// Значение переменной e это число 2
+
+// Change code below this line
+const a = 3 % 3;
+const b = 4 % 3;
+const c = 11 % 4;
+const d = 12 % 7;
+const e = 8 % 3;
+
+// 2-29
+// Напиши функцию getEvenNumbers(start, end) которая возвращает массив всех чётных чисел от start до end. 
+// Чётным считается число которое делится на 2 без остатка(10 % 2 === 0).
+
+function getEvenNumbers(start, end) {
+   // Change code below this line
+const evenNumbers = [];
+  for (i = start; i <= end; i +=1) {
+   if (i%2 === 0) {
+     evenNumbers.push(i);
+  }
+  }
+  
+  return evenNumbers;
+    // Change code above this line
+  }
+
+getEvenNumbers(6, 12);
+getEvenNumbers(7, 7);
+
+// 2-30
+// Дополни код так, чтобы в переменную number записывалось первое число от start до end, которое делится на 5 без остатка.
+// Объявлена переменная start со значением 6
+// Объявлена переменная end со значением 27
+// Объявлена переменная number без инициализации
+// Итоговое значение переменной number равно 10
+// В цикле for используется break для выхода до завершения всех итераций цикла
+
+const start = 6;
+const end = 27;
+let number;
+
+for (let i = start; i < end; i += 1) {
+  if (i % 5 === 0) {
+    number = i;
+    break;
+  }
+}
+
+// 2-31
+// Выполни рефакторинг функции findNumber(start, end, divisor) так, чтобы она:
+
+// возвращала первое число от start до end, которое делится на divisor без остатка
+// не использовала оператор break
+// не использовала переменную number
+// Tests:
+// Объявлена функция findNumber(start, end, divisor)
+// Вызов findNumber(2, 6, 5) возвращает 5
+// Вызов findNumber(8, 17, 3) возвращает 9
+// Вызов findNumber(6, 9, 4) возвращает 8
+// Вызов findNumber(16, 35, 7) возвращает 21
+// Вызов findNumber() со случайным набором чисел возвращает верный результат
+// В цикле for не должен использоваться break для выхода до завершения всех итераций цикла
+
+function findNumber(start, end, divisor) {
+  // Change code below this line
+  let number;
+
+  for (let i = start; i < end; i += 1) {
+    if (i % divisor === 0) {
+      number = i;
+      return number;
+    }
+  }  
+  // Change code above this line
+}
+
+// 2-32
+// Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) - проверяет,
+// есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
+
+// При выполнении этой задачи в теле функции includes() нельзя использовать метод массив.includes(значение).
+
+// Tests:
+// Объявлена функция includes(array, value)
+// Вызов includes([1, 2, 3, 4, 5], 3) возвращает true
+// Вызов includes([1, 2, 3, 4, 5], 17) возвращает false
+// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") возвращает true
+// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") возвращает false
+// Вызов includes(["apple", "plum", "pear", "orange"], "plum") возвращает true
+// Вызов includes(["apple", "plum", "pear", "orange"], "kiwi") возвращает false
+// Вызов includes() для случайного массива со случайным value возвращает верный boolean
+// В функции includes используется for, return, но не метод массива includes
+
+function includes(array, value) {
+  // Change code below this line
+for (i=0; i < array.length; i++) {
+  if (array[i] === value) {
+    return true;
+  }
+}
+  return false;
+  // Change code above this line
+}
--- test.js
+++ test.js
@@ -1,40 +1,55 @@
-function getExtremeElements(array) {
-  // Change code below this line
- const newArray = [array[0], array[array.length - 1]];
-return newArray;
+// function getExtremeElements(array) {
+//   // Change code below this line
+//  const newArray = [array[0], array[array.length - 1]];
+// return newArray;
   
 
-  // Change code above this line
-}
+//   // Change code above this line
+// }
 
-console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+// console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
 
-console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
+// console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
 
 
-const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
 
-// Change code below this line
-const firstTwoEls = fruits.slice(0, 2);
-const nonExtremeEls = fruits.slice(1, fruits.length - 1);
-const lastThreeEls = fruits.slice(-3);
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, fruits.length - 1);
+// const lastThreeEls = fruits.slice(-3);
 
-console.log(firstTwoEls);
-console.log(nonExtremeEls);
-console.log(lastThreeEls);
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
 
-function calculateTotal(number) {
- // Change code below this line
+// function calculateTotal(number) {
+//  // Change code below this line
 
-  let total = 0;
+//   let total = 0;
   
-  for (let i = 1; i <= number; i += 1) { 
+//   for (let i = 1; i <= number; i += 1) { 
   
-    total += i;
-}
+//     total += i;
+// }
 
-  return total;
-  // Change code above this line
-}
+//   return total;
+//   // Change code above this line
+// }
 
-console.log(calculateTotal(8));
+// console.log(calculateTotal(8));
+
+const string = "May the force be with you";
+
+const array = string.split(" ");
+console.log(array);
+let longestWord = array[0];
+
+
+for (i = 0; i < array.length; i +=1) {
+    if (longestWord.length < array[i].length) {
+      longestWord = array[i];
+    }
+}
+  
+  console.log(longestWord);
